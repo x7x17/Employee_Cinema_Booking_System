bc2e25e8276a4fb66506144b6d64e914108be5fb
x7x17 <x7x17@students.keele.ac.uk>
2023-03-08 22:31:30 +0000

    Create Code for Booking System.py
    Created the first file for the system. Began hard coding the Movie Titles dictionary but suggest reviewing this maybe using a CSV to import the dictionary or using OOP to create classes for Movies for example attributes would be Title, age rating, runtime, price.

---
+++ Code for Booking System.py
@@ -0,0 +1,13 @@
+
+# Dictionary to store movies
+
+movieTitles = {'A Fist Full of Dollar': [15, '1h 39m', 6], 
+              'Borat: Cultural Learnings of America for Make Benefit Glorious Nation of Kazakhstan': [15, '1h 24m', 6]
+              
+              
+              
+              
+               }
+
+
+print(movieTitles)
