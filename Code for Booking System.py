32840e71a252aa68eafe5102e2c099fef728e170
x7x17 <x7x17@students.keele.ac.uk>
2023-03-09 12:43:23 +0000

    Update Code for Booking System.py

--- Code for Booking System.py
+++ Code for Booking System.py
@@ -1,5 +1,9 @@
 
 # Dictionary to store movies
+#Rather than hard coding we should import by CSV 
+
+import csv
+
 
 movieTitles = {'A Fist Full of Dollar': [15, '1h 39m', 6], 
               'Borat: Cultural Learnings of America for Make Benefit Glorious Nation of Kazakhstan': [15, '1h 24m', 6]
@@ -10,4 +14,7 @@ movieTitles = {'A Fist Full of Dollar': [15, '1h 39m', 6],
                }
 
 
+
 print(movieTitles)
+
+#TKinter GUI for Booking Confirmation Screen
