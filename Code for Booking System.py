c90fb909ea97c047001a57ced7dcd6ae53032fd0
Jeffreytan <66024135+yeehentan@users.noreply.github.com>
2023-03-14 16:00:33 +0000

    Update Code for Booking System.py

--- Code for Booking System.py
+++ Code for Booking System.py
@@ -1,12 +1,11 @@
 
 # Dictionary to store movies
 #Rather than hard coding we should import by CSV 
-
-
 import csv
 
 available_seats = []
 booked_seats = []
+customer_order = []
 for i in range (21):
     available_seats.append(i)
 
@@ -38,7 +37,10 @@ def check_showtime(movieDict):
     response2 = int(input('please key in the time ID:'))-1
 
     print('you have selected to watch a ',movieDict[response1][0], ' at the following time: ',movieDict[response1][5][response2])
-
+    customer_order.append(response1)
+    customer_order.append(movieDict[response1][0])
+    
+    customer_order.append(movieDict[response1][5][response2])
 def select_seat(booked_seats):
     print('please select a seat from the following ID')
     for i in available_seats:
@@ -47,6 +49,7 @@ def select_seat(booked_seats):
     if response3 not in booked_seats:
         print(response3, 'selected')
         booked_seats.append(response3)
+        customer_order.append(response3)
     else:
         print('seat occuppied')
     return booked_seats
@@ -56,3 +59,4 @@ a = read_csv("Software Engineering - Movie List.csv")
 show_movie(a)
 check_showtime(a)
 booked_seats = select_seat(booked_seats)
+print(customer_order)
