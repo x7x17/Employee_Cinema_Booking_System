27ac9e7cbf7de6d92d839a962249e1ea6937463a
Jeffreytan <66024135+yeehentan@users.noreply.github.com>
2023-03-14 15:07:40 +0000

    Update Code for Booking System.py

--- Code for Booking System.py
+++ Code for Booking System.py
@@ -2,17 +2,20 @@
 # Dictionary to store movies
 #Rather than hard coding we should import by CSV 
 
+
 import csv
 
 def read_csv(name):
     movieDict = {}
     with open(name, 'r', newline='') as file:
         csvreader = csv.reader(file)
-        
+        counter = 0
         for row in csvreader:
-            k = row[0]
-            v=[row[1],row[2],row[3],row[4],row[5],[row[6],row[7],row[8]]]
-            movieDict[k]=v
+            if counter != 0:
+                k = row[0]
+                v=[row[1],row[2],row[3],row[4],row[5],[row[6],row[7],row[8]]]
+                movieDict[k]=v
+            counter += 1
             
         return movieDict
       
@@ -22,8 +25,10 @@ def show_movie(movieDict):
         print(k,v[0])          
 
 
-'''def check_showtime:
-    response = input('please key in the ID of the movie')
-'''    
+def check_showtime(movieDict):
+    response = input('please key in the ID of the movie: ')
+    print(movieDict[response])
+
 a = read_csv("Software Engineering - Movie List.csv")
 show_movie(a)
+check_showtime(a)
