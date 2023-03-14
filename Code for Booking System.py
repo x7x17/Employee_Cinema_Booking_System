8787d4cac97483b6eaaebe2e862dd49c5cc115eb
Jeffreytan <66024135+yeehentan@users.noreply.github.com>
2023-03-14 14:55:27 +0000

    Update Code for Booking System.py

--- Code for Booking System.py
+++ Code for Booking System.py
@@ -4,50 +4,26 @@
 
 import csv
 
-
-
-
-
-#Function to read csv file and place values which are in more than one column into a list
-def new_dict1(name):
-    dict1 = {}
-    
-    with open(name, mode="r") as doc:
-        for row in csv.reader(doc):
-            nlist = []
-            for x in row:
+def read_csv(name):
+    movieDict = {}
+    with open(name, 'r', newline='') as file:
+        csvreader = csv.reader(file)
+        
+        for row in csvreader:
+            k = row[0]
+            v=[row[1],row[2],row[3],row[4],row[5],[row[6],row[7],row[8]]]
+            movieDict[k]=v
             
-                if x != '':
-                    nlist.append(x)
-            
-            for i in nlist:
-                dict1[nlist[0].upper()] = nlist[1:]
-
-    return dict1
-
-
-movieTitles = new_dict1('Software Engineering - Movie List.csv')
+        return movieDict
+      
+def show_movie(movieDict):
+    print('ID','Movie title')
+    for k,v in movieDict.items():
+        print(k,v[0])          
 
 
-for i in movieTitles:
-    print(i)
-
-
-'''
-movieTitles = {'A Fist Full of Dollar': [15, '1h 39m', 6], 
-              'Borat: Cultural Learnings of America for Make Benefit Glorious Nation of Kazakhstan': [15, '1h 24m', 6]
-              
-             
-              
-               }
-
-'''
-
-
-
-
-
-
-print(movieTitles)
-
-#TKinter GUI for Booking Confirmation Screen
+'''def check_showtime:
+    response = input('please key in the ID of the movie')
+'''    
+a = read_csv("Software Engineering - Movie List.csv")
+show_movie(a)
