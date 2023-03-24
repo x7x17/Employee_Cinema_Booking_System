3c884e9b0c81df3eff0d52430c7df109bb431303
x7x17 <x7x17@students.keele.ac.uk>
2023-03-24 23:24:38 +0000

    Completed the core functionality of the sys
    Merged the front and backend together. Still work to do but all the core features have been achieved. Refinements need to be made such as avoiding the seats being selected twice. Also maybe rather than using a dictionary we could use a Database or CSV to record previous orders as currently when the application is closed all data is overwritten.

--- Code for Booking System V2.py
+++ Code for Booking System V2.py
@@ -7,6 +7,8 @@ available_seats = []
 customer_order = []
 booked_seats = {}
 
+orderSummary = []
+
 for i in range (21):
     available_seats.append(i)
 
@@ -25,14 +27,7 @@ def read_csv(name):
             counter += 1     
         return movieDict
       
-def show_movie(movieDict):
-    print('MovieID','Movie title')
-    temp = []
-    for k,v in movieDict.items():
-        if v[0] not in temp:
-            print(v[0],v[1])         
-            temp.append(v[0])
-    temp.clear()
+
 
 def check_showtime(movieDict):
     response1 = input('please key in the ID of the movie: ')
@@ -64,72 +59,313 @@ def select_seat(booked_seats):
             print('seat ',response3, ' booked successfully')         
             
 
-a = read_csv("Software Engineering - Movie List.csv")
-show_movie(a)
-check_showtime(a)
-select_seat(booked_seats)
+#a = read_csv("Software Engineering - Movie List.csv")
+movieDict = read_csv("Software Engineering - Movie List.csv")
 
 
-
-show_movie(a)
-check_showtime(a)
-select_seat(booked_seats)
-
-
-
+from tkinter import *
 import tkinter as tk
-from tkinter import ttk
+from tkinter.ttk import Combobox
+
+def movieSelect():
 
 
-class CinemaSeatSelection:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Cinema Seat Selection")
-        self.master.geometry("2500x1800")
+    frame = Frame(root)
 
-        # Creating a frame to hold the widgets
-        self.frame = tk.Frame(self.master)
-        self.frame.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
+    frame = Frame(root, width=1000, height=500, bg='red')
+    #frame.place(x=0, y=0)
+    #frame.pack(fill="both", expand=True)
 
-        # Adding a title to the window
-        self.title_label = tk.Label(self.frame, text="Cinema Seat Selection", font=("Helvetica", 18, "bold"))
-        self.title_label.pack(pady=(0, 20))
+     # Create the heading label
+    heading_label = Label(frame, text="List of Movies",height=1, width=80, font=("Ariel", 12))
+    heading_label.pack(pady=10)
 
-        # Adding an image to the window
-        self.seat_image = tk.PhotoImage(file="cinemaSeats.png")
-        self.image_label = tk.Label(self.frame, image=self.seat_image)
-        self.image_label.pack()
+    #create movie select buttons
+    #movieDict = read_csv("Software Engineering - Movie List.csv")
+    temp = []
+    movie_options = []
+    for k,v in movieDict.items():
+        if v[0] not in temp:
+           # print(v[0],v[1])   
+            movie_list = Label(frame, text=f"{v[1]}",font=("Ariel", 10), height=1, width=70)
+            movie_list.pack(pady=5)
+            movie_options.append(v[1])
+            temp.append(v[0])
+    temp.clear()
 
-        # Adding a label for seat selection
-        self.select_label = tk.Label(self.frame, text="Select a seat:", font=("Helvetica", 12))
-        self.select_label.pack(pady=(20, 10))
+    # Adding a dropdown menu for Movie selection
+    selected_movie = StringVar()
+    selected_movie.set(movie_options[0])
+    movie_dropdown = Combobox(frame, textvariable=selected_movie, values=movie_options, state="readonly")
+    movie_dropdown.pack()
 
-        # Adding a dropdown menu for seat selection
-        self.seat_options = [str(i) for i in range(1, 21)]
-        self.selected_seat = tk.StringVar()
-        self.selected_seat.set(self.seat_options[0])
-        self.seat_dropdown = ttk.Combobox(self.frame, textvariable=self.selected_seat, values=self.seat_options,
-                                          state="readonly")
-        self.seat_dropdown.pack()
+    def s1ToS2():
 
-        # Adding a button to submit the seat selection
-        self.submit_button = tk.Button(self.frame, text="Submit", command=self.submit_seat_selection)
-        self.submit_button.pack(pady=20)
+        a =movie_dropdown.get()
+        orderSummary.append(a)
+        #print(orderSummary)
+        home_frame.destroy()
 
-        # Adding a label for displaying seat selection message
-        self.message_label = tk.Label(self.frame, text="", font=("Helvetica", 12))
-        self.message_label.pack(pady=10)
+        showTime_frame = showingTimes()
 
-    def submit_seat_selection(self):
-        # Retrieving the selected seat
-        selected_seat = self.selected_seat.get()
+        showTime_frame.pack(fill="both", expand=True)
 
-        # Updating the message label with the selected seat number
-        message = f"You have selected seat {selected_seat}"
-        self.message_label.config(text=message)
+    
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = CinemaSeatSelection(root)
-    root.mainloop()
+    '''
+    # Create the movie buttons
+    for i in range(10):
+        movie_button = Button(frame, text=f"Movie {i+1}",font=("Ariel", 10), height=1, width=30)
+        movie_button.pack(pady=5)
+    '''
+    # Create the contact details button
+    contact_num = Label(frame, text="Phone Number: 0998393883939 \n Email: Vuecinema@xyz.com",height=3, width=60, font=("Ariel", 9))
+    contact_num.pack(pady=10)
+
+
+    Submitbtn = Button(frame, text = 'Submit', command = s1ToS2)
+    Submitbtn.pack(pady=30)
+
+    return frame
+
+print(orderSummary)
+
+def showingTimes():
+    
+    showTime_frame = Frame(root)
+
+    showTime_frame = Frame(root, width=1000, height=500, bg='blue')
+
+     # Create the heading label
+    heading_label = Label(showTime_frame, text="Showing Times",height=1, width=80, font=("Ariel", 12))
+    heading_label.pack(pady=10)
+
+
+    # Create the movie buttons
+    timeSlotOptions = []
+    for k,v in movieDict.items():
+        if v[1] == orderSummary[0]:
+            movie_button = Button(showTime_frame, text=f"Time Slot {v[6]}",font=("Ariel", 10), height=1, width=30)
+            movie_button.pack(pady=5)
+            timeSlotOptions.append(v[6])
+
+         
+    # Adding a dropdown menu for Time Slot selection
+    selected_TimeSlot = StringVar()
+    selected_TimeSlot.set(timeSlotOptions[0])
+    timeSlot_dropdown = Combobox(showTime_frame, textvariable=selected_TimeSlot, values=timeSlotOptions, state="readonly")
+    timeSlot_dropdown.pack()
+
+
+    #create back button to go back to the previous screen needs to be called within each screen function
+    '''def back():
+            showTime_frame.destroy()
+            home_frame = movieSelect()
+            home_frame.pack(fill=NONE, expand=0)
+    backBtn =Button(showTime_frame, text='Back', font=('Arial', 25), command=back)
+    backBtn.pack(pady=10)
+    '''
+    
+    #Back button currently has errors relating to not unpacking the previous screen and also packing the 
+    #backBtn each time the backBtn is pressed
+    
+
+
+    def s2ToS3():
+        a = timeSlot_dropdown.get()
+        orderSummary.append(a)
+
+        showTime_frame.destroy()
+
+        seatSelect_frame = seatSelect()
+        seatSelect_frame.pack(fill=NONE, expand=0)
+
+        #for loop to check if we are able to verfy the info selected by the user to the dictionary
+        for k,v in movieDict.items():
+            if v[1] == orderSummary[0] and v[6] == orderSummary[1]:
+                print ('success')
+
+    
+    Submitbtn = Button(showTime_frame, text = 'Submit', command = s2ToS3)
+    Submitbtn.pack(pady=30)
+
+
+    return showTime_frame
+
+
+def seatSelect():
+
+    
+    seatSelect_frame = Frame(root)
+
+
+    seatSelect_frame = Frame(root, width=1000, height=500, bg='green')
+    #frame.place(x=0, y=0)
+    #frame.pack(fill="both", expand=True)
+
+    # Create the heading label
+    heading_label = Label(seatSelect_frame, text="Seat Selection",height=1, width=80, font=("Ariel", 12))
+    heading_label.pack(pady=10)
+
+    # Adding a title to the window
+    
+
+    # Adding an image to the window
+    seat_image = PhotoImage(file="cinemaSeats.png")
+    image_label = Label(seatSelect_frame, image=seat_image)
+    image_label.pack()
+
+    # Adding a label for seat selection
+    select_label = Label(seatSelect_frame, text="Select a seat:", font=("Helvetica", 12))
+    select_label.pack(pady=(20, 10))
+
+    # Adding a dropdown menu for seat selection
+    seat_options = [str(i) for i in range(1, 21)]
+    selected_seat = StringVar()
+    selected_seat.set(seat_options[0])
+    seat_dropdown = Combobox(seatSelect_frame, textvariable=selected_seat, values=seat_options, state="readonly")
+    seat_dropdown.pack()
+    
+    
+    #This code block doesn't work it just displays you have selected seat 1 constantly 
+
+    '''
+     #Adding a label for displaying seat selection message
+    message_label = Label(seatSelect_frame, text="", font=("Helvetica", 12))
+    message_label.pack(pady=10)
+    def display_choosen_seat():
+    # Retrieving the selected seat
+    choosen_seat = selected_seat.get()
+    # Updating the message label with the selected seat number
+        message = f"You have selected seat {choosen_seat}"
+        message_label.config(text=message)
+    '''
+    
+
+    def s3ToS4():
+
+        a = seat_dropdown.get()
+        orderSummary.append(a)
+        
+        seatSelect_frame.destroy()
+
+        confirmPage_frame = confirmPage()
+        confirmPage_frame.pack(fill=NONE, expand=0)
+
+    
+    #Submitbtn = Button(seatSelect_frame, text = 'Submit', command=lambda: [s3ToS4(), submit_seat_selection()])
+    Submitbtn = Button(seatSelect_frame, text = 'Submit', command= s3ToS4) 
+    Submitbtn.pack(pady=30)
+
+   
+
+   
+    
+    return seatSelect_frame
+
+
+def confirmPage():
+
+    confirmPage_frame = Frame(root)
+
+
+    confirmPage_frame = Frame(root, width=1000, height=500, bg='purple')
+
+    # Create the heading label
+    heading_label = Label(confirmPage_frame, text="Confirmation Page",height=1, width=80, font=("Ariel", 12))
+    heading_label.grid(row=0, column=1, sticky="news", padx=0, pady=0)
+
+
+    listBox = Listbox(confirmPage_frame, width=80, height=12)
+    index = tk.END
+    listBox.insert(index,'Movie:'+orderSummary[0])
+    listBox.insert(index,'Time: '+orderSummary[1])
+    listBox.insert(index, 'Seat number: '+orderSummary[2])
+    listBox.grid(row=1, column=1, sticky="news", padx=20, pady=10)
+
+
+    user_info_LabelFrame =LabelFrame(confirmPage_frame, text ="Customer Information")
+    user_info_LabelFrame.grid(row=2, column=1, padx =20, pady = 10)
+
+    title_label = Label(user_info_LabelFrame , text="Title")
+    title_combobox = Combobox(user_info_LabelFrame , values=["", "Mr.","Miss", "Ms", "Mrs.","Dr.","Master"])
+
+    title_label.grid(row=0, column=0)
+    title_combobox.grid(row=1, column =0)
+
+
+    first_name_label =Label(user_info_LabelFrame , text ="First Name")
+    first_name_label.grid(row=0, column=1)
+    last_name_label = Label(user_info_LabelFrame , text ="Last Name")
+    last_name_label.grid(row=0, column=2)
+
+    first_name_entry= Entry(user_info_LabelFrame )
+    last_name_entry = Entry(user_info_LabelFrame )
+
+    first_name_entry.grid(row=1, column=1,)
+    last_name_entry.grid(row =1, column =2)
+
+
+
+    age_label = Label(user_info_LabelFrame , text ="Age")
+    age_spinbox =Spinbox(user_info_LabelFrame , from_=12, to =150)
+
+    age_label.grid(row=2, column=0)
+    age_spinbox.grid(row=3, column =0)
+
+    sex_label= Label(user_info_LabelFrame , text = "Sex")
+    sex_combobox =Combobox(user_info_LabelFrame , values=["Male","Female"])
+
+    sex_label.grid(row=2, column= 1)
+    sex_combobox.grid(row=3, column=1)
+
+
+    email_label =Label(user_info_LabelFrame , text ="Email Address")
+    email_label.grid(row=2, column=2,)
+
+    email_entry= Entry(user_info_LabelFrame )
+    email_entry.grid(row=3, column=2,)
+
+
+    for widget in user_info_LabelFrame .winfo_children():
+        widget.grid_configure(padx=10,pady=5)
+
+
+    def enter_data():
+
+        a = title_combobox.get()
+        b= first_name_entry.get()
+        c = last_name_entry.get()
+        d= age_spinbox.get()
+        e = email_entry.get()
+        orderSummary.append(a)
+        orderSummary.append(b)
+        orderSummary.append(c)
+        orderSummary.append(d)
+        orderSummary.append(e)
+        print(orderSummary)
+    button =Button(confirmPage_frame, text="Submit" , command= enter_data)
+    button.grid(row=3, column=1, sticky="news", padx=20, pady=10)
+
+    return confirmPage_frame
+
+
+
+# Create a new frame that displays the finally details alongside the price to pay and display payment details i..e paypal account or cash amount
+#A button should be avaible to return the user to the home_frame
+#def successScreen():
+
+ 
+
+ 
+
+root = Tk()
+
+root.minsize(height=500, width=1000)
+
+home_frame = movieSelect()
+
+home_frame.pack(fill=NONE, expand=0)
+
+root.mainloop()
